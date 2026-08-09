e2ac9a75437cf024d0c787c5a47b5a89106cbae9
Azzaza <emmanuelokolie20@gmail.com>
2026-08-09 05:23:39 +0100

    Update check_annabet_gp.py

--- check_annabet_gp.py
+++ check_annabet_gp.py
@@ -18,7 +18,7 @@ HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Connection": "keep-alive",
     "Referer": "https://annabet.com/en/soccerstats/",
     "Sec-Fetch-Dest": "document",
