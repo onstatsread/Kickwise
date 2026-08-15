61d068e3825b6ade1fd27734e462e3d61ef2b7ee
Azzaza <emmanuelokolie20@gmail.com>
2026-08-15 20:32:29 +0100

    Add POST method to CORS middleware

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Kickwise API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["GET"],
+    allow_methods=["GET", "POST"],
     allow_headers=["*"],
 )
 
